f2de1b0ec79f4999a6ec5a35ef6f283117166d06
Emma-Marie <emma-marie@outlook.dk>
2023-04-16 20:59:16 +0200

    plotting function created (16th)

--- src/emotion_clf.py
+++ src/emotion_clf.py
@@ -3,19 +3,28 @@ import pandas as pd
 # import pipelines
 from transformers import pipeline
 import random2 as rd
+import matplotlib.pyplot as plt
 
 def get_data():
     # define path
     data_path = os.path.join("in","fake_or_real_news.csv")
     # load data
     news_data = pd.read_csv(data_path, index_col=0) 
-    # create list of only the headliens
+    # create list of only the headlines
     headlines = news_data["title"]
+    # create list of only FAKE headlines
+    fake_headlines = []
+    for label in news_data: 
+        if label == "FAKE":
+            #if this, then grap the "title" from the same row
+            fake_headlines.append(news_data["title"])
+        
+    # create list of only REAL headlines
+
     #sampling random headliens --> REMOVE BEFORE HAND IN
     headlines = rd.sample(headlines, 10)
-    #headlines = news_data["title"]
 
-    return headlines
+    return headlines, fake_headlines
 
 def emotion_clf(headlines):
     # load emotion pipeline from Hugging Face
@@ -29,22 +38,39 @@ def emotion_clf(headlines):
     
     return labels
     
+def plot_emotions(labels):
+    names=["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
+    values=[labels.count("anger"),
+            labels.count("disgust"), 
+            labels.count("fear"), 
+            labels.count("joy"), 
+            labels.count("neutral"), 
+            labels.count("sadness"),
+            labels.count("surprise")]
+    bar_plot = plt.bar(names, values)
+    plt.suptitle('Destribution of emotions across all headlines')
+    return bar_plot
 
 def main():
     #get headlines
-    headlines = get_data()
+    headlines, fake_headlines = get_data()
     print("Headlines ready!")
+    print(fake_headlines)
     # predict emotions in headlines
     labels = emotion_clf(headlines)
     print("Labels for headlines predicted")
-    # Create matrix showin results 
-    dataframes = []
-    dataframe = pd.DataFrame(labels, columns=["Headlines", "anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"])
-    dataframes.append(dataframe)
-    emotion_data = pd.concat(dataframes)
+    # creating the bar plot
+    bar_plot = plot_emotions(labels)
+    plt.savefig('out/all_headliens.png',dpi=400)
+    
+    ### Create matrix showin results 
+    #dataframes = []
+    #dataframe = pd.DataFrame(labels, columns=["Headlines", "anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"])
+    #dataframes.append(dataframe)
+    #emotion_data = pd.concat(dataframes)
 
     #save dataframe as csv
-    folderdata.to_csv(f'out/labels.csv', index=False)
+    #folderdata.to_csv(f'out/labels.csv', index=False)
 
     print(f"Dataframe is saved")
 
