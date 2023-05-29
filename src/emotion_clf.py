efbd2f6eb9537b5408f00eb5eed370d4704f189b
Emma-Marie <emma-marie@outlook.dk>
2023-05-29 17:57:14 +0200

    script more tidy

--- src/emotion_clf.py
+++ src/emotion_clf.py
@@ -4,13 +4,14 @@ import pandas as pd
 from transformers import pipeline
 import matplotlib.pyplot as plt
 
-def get_data():
-    # define path
-    data_path = os.path.join("in","fake_or_real_news.csv")
+path_fake_real_news = os.path.join("in","fake_or_real_news.csv")
+
+def get_data(data_path):
     # load data
     news_data = pd.read_csv(data_path, index_col=0) 
     # create list of only the headlines
     headlines = news_data["title"]
+    print("Data is ready")
 
     return headlines, news_data
 
@@ -31,10 +32,23 @@ def emotion_clf(headlines, news_data):
     # data frame for emotions in READL headlines
     fake_news_mask = news_data["label"]=="FAKE"
     fake_emotions = total_emotions.loc[fake_news_mask].reset_index(drop=True).rename("Emotion")
+    print("Headlines are classified")
 
     return total_emotions, real_emotions, fake_emotions
 
-def plot_emotions(input_emotions):
+def emotion_table(input_emotions, cathegory_name):
+    input_emotion_table = pd.crosstab(index=input_emotions, columns="Count")
+    table_outpath = os.path.join("out", f"{cathegory_name}_table.csv")
+    input_emotion_table.to_csv(table_outpath)
+    print("Emotion tables are saved")
+
+def save_plot(category_name):
+    plt.title(f"Distribution of emotions across {category_name}")
+    outpath = os.path.join("out", f"{category_name}_bars.png")
+    plt.savefig(outpath, dpi=400)
+    plt.clf() # clear figure
+
+def plot_emotions(input_emotions, category_name):
     names=["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
     anger_count = 0
     disgust_count = 0
@@ -61,40 +75,35 @@ def plot_emotions(input_emotions):
         else: 
             pass
     values = [anger_count, disgust_count, fear_count, joy_count, neutral_count, sadness_count, surprise_count]
+    # plot the values
     plt.bar(names, values)
     plt.xlabel("Emotion")
     plt.ylabel("Count")
+    # save bar plot
+    save_plot(category_name)
+    print("Bar chards are saved")
 
 def main():
     #get headlines
-    headlines, news_data = get_data()
+    headlines, news_data = get_data(path_fake_real_news)
     # predict emotions in headlines
     total_emotions, real_emotions, fake_emotions = emotion_clf(headlines, news_data)
 
-    # create and save table for emotions across all headlines
-    all_headlines_table = pd.crosstab(index=total_emotions, columns="Count")
-    all_headlines_table.to_csv("out/all_headlines_table.csv")
-    # table for emotions across REAL headlines
-    real_headlines_table = pd.crosstab(index=real_emotions, columns="Count")
-    real_headlines_table.to_csv("out/real_headlines_table.csv")
-    # table for emotions across FAKE headlines
-    fake_headlines_table = pd.crosstab(index=fake_emotions, columns="Count")
-    fake_headlines_table.to_csv("out/fake_headlines_table.csv")
+    # create and save table for emotions across ...
+    # ...all headlines
+    category_total = "total_emotions"
+    emotion_table(total_emotions, category_total)
+    # ... REAL headlines
+    category_real = "real_emotions"
+    emotion_table(real_emotions, category_real)
+    # ... FAKE headlines
+    category_fake = "fake_emotions"
+    emotion_table(fake_emotions, category_fake)
     
-    # plot_emotions for all headlines
-    plot_emotions(total_emotions)
-    plt.title("Distribution of emotions across all headlines")
-    plt.savefig("out/all_headlines_bars.png",dpi=400)
-    plt.clf() # clear figure
-    # plot for REAl headlines
-    plot_emotions(real_emotions)
-    plt.title("Distribution of emotions across real headlines")
-    plt.savefig("out/real_headlines_bars.png",dpi=400)
-    plt.clf() # clear figure
-    # plot for FAKE headlines
-    plot_emotions(fake_emotions)
-    plt.title("Distribution of emotions across fake headlines")
-    plt.savefig("out/fake_headlines_bars.png",dpi=400)
+    # create bar plots for all headlines, REAl headlines and FAKE headlines
+    plot_emotions(total_emotions, category_total)
+    plot_emotions(real_emotions, category_real)
+    plot_emotions(fake_emotions, category_fake)
 
 if __name__ == "__main__":
     main()
